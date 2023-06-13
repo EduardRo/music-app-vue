51770645e7d2a157d25a005cb75d12c950d30c1b
EduardRo <eduardstancu@yahoo.com>
2023-06-13 11:39:17 +0300

    save101

--- src/stores/model.js
+++ src/stores/model.js
@@ -4,5 +4,10 @@ export default defineStore("modal",{
     state:()=>({
         isOpen:false,
         numberOf:1,
-    })
+    }),
+    getters:{
+        hiddenClass(state){
+            return !state.isOpen? "hidden": "";
+        }
+    }
 });
